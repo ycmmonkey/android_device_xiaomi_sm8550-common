2245cfef5ef6a24692ac417d2f7ffa63b8a80ef2
Arian <arian.kulmer@web.de>
2024-06-02 03:29:32 +0200

    sm8550-common: Add nonui notifier
    Change-Id: Id447dec0d6c6a384a55a25db45f2d763ba23635a

--- common.mk
+++ common.mk
@@ -363,6 +363,7 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.xiaomi
+    nonui-notifier
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml

e4afd4cbf825c6babc1df491e9c4f1a40a4e2be8
Arian <arian.kulmer@web.de>
2026-04-05 12:09:53 -0500

    sm8550-common: Add a condition for udfps
    There are devices that don't use udfps, so we'll add a condition to ensure that only
    devices that support the feature can use it.
    
    Change-Id: Idbe276b4399e6ae38bac72e891edfa8d6a2e2371
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- common.mk
+++ common.mk
@@ -154,6 +154,14 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi \
     libudfpshandler
 
+ifeq ($(TARGET_HAS_UDFPS),true)
+PRODUCT_PACKAGES += \
+    FrameworkResOverlayUdfps
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.sensors.xiaomi.udfps=true
+endif
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
